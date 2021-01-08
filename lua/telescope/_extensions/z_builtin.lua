8d1f7fa8c952d8797f48001bd423a6ed2a6dbc5d
delphinus <me@delphinus.dev>
2021-01-08 20:10:23 +0900

    Add actions to find_files & cd & lcd

--- lua/telescope/_extensions/z_builtin.lua
+++ lua/telescope/_extensions/z_builtin.lua
@@ -1,8 +1,14 @@
+local actions = require'telescope.actions'
 local conf = require'telescope.config'.values
 local finders = require'telescope.finders'
 local pickers = require'telescope.pickers'
 
 local M = {}
+
+local function parse_entry(value)
+  local score, dir = value:match'([%.%d]+)%s+(.+)'
+  return {score = score, dir = dir}
+end
 
 M.list = function(opts)
   opts = opts or {}
@@ -13,6 +19,29 @@ M.list = function(opts)
       opts
     ),
     sorter = conf.file_sorter(opts),
+    attach_mappings = function()
+      actions.goto_file_selection_edit:replace(function(prompt_bufnr)
+        local selection = actions.get_selected_entry()
+        actions.close(prompt_bufnr)
+        local result = parse_entry(selection.value)
+        require'telescope.builtin'.find_files{cwd = result.dir}
+      end)
+      actions.goto_file_selection_split:replace(function(prompt_bufnr)
+        local selection = actions.get_selected_entry()
+        actions.close(prompt_bufnr)
+        local result = parse_entry(selection.value)
+        vim.cmd('cd '..result.dir)
+        print('chdir to '..result.dir)
+      end)
+      actions.goto_file_selection_vsplit:replace(function(prompt_bufnr)
+        local selection = actions.get_selected_entry()
+        actions.close(prompt_bufnr)
+        local result = parse_entry(selection.value)
+        vim.cmd('lcd '..result.dir)
+        print('lchdir to '..result.dir)
+      end)
+      return true
+    end,
   }):find()
 end
 
