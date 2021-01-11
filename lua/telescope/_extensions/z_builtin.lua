b483f30d2ae445e4c6a016acb2d9916324e6f7d8
delphinus <me@delphinus.dev>
2021-01-11 22:51:36 +0900

    Simplify logic to replace original functions
    See https://github.com/delphinus/telescope-z.nvim/commit/8d1f7fa8c952d8797f48001bd423a6ed2a6dbc5d#commitcomment-45791096

--- lua/telescope/_extensions/z_builtin.lua
+++ lua/telescope/_extensions/z_builtin.lua
@@ -18,26 +18,20 @@ M.list = function(opts)
     prompt_title = 'Visited directories from z',
     finder = finders.new_oneshot_job(opts.z_command_list, opts),
     sorter = conf.file_sorter(opts),
-    attach_mappings = function()
-      actions.goto_file_selection_edit:replace(function(prompt_bufnr)
+    attach_mappings = function(prompt_bufnr)
+      actions._goto_file_selection:replace(function(_, cmd)
         local selection = actions.get_selected_entry()
         actions.close(prompt_bufnr)
         local result = parse_entry(selection.value)
-        require'telescope.builtin'.find_files{cwd = result.dir}
-      end)
-      actions.goto_file_selection_split:replace(function(prompt_bufnr)
-        local selection = actions.get_selected_entry()
-        actions.close(prompt_bufnr)
-        local result = parse_entry(selection.value)
-        vim.cmd('cd '..result.dir)
-        print('chdir to '..result.dir)
-      end)
-      actions.goto_file_selection_vsplit:replace(function(prompt_bufnr)
-        local selection = actions.get_selected_entry()
-        actions.close(prompt_bufnr)
-        local result = parse_entry(selection.value)
-        vim.cmd('lcd '..result.dir)
-        print('lchdir to '..result.dir)
+        if cmd == 'edit' then
+          require'telescope.builtin'.find_files{cwd = result.dir}
+        elseif cmd == 'new' then
+          vim.cmd('cd '..result.dir)
+          print('chdir to '..result.dir)
+        elseif cmd == 'vnew' then
+          vim.cmd('lcd '..result.dir)
+          print('lchdir to '..result.dir)
+        end
       end)
       return true
     end,
