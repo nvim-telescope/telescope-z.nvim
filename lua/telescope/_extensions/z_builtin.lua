0e4b7d327a95be2415942dc97061f1e22c70d9d0
delphinus <me@delphinus.dev>
2021-01-21 13:20:51 +0900

    Enable to shorten/tail paths

--- lua/telescope/_extensions/z_builtin.lua
+++ lua/telescope/_extensions/z_builtin.lua
@@ -3,9 +3,12 @@ local conf = require'telescope.config'.values
 local entry_display = require'telescope.pickers.entry_display'
 local finders = require'telescope.finders'
 local from_entry = require'telescope.from_entry'
+local path = require'telescope.path'
 local pickers = require'telescope.pickers'
 local previewers = require'telescope.previewers.term_previewer'
 local utils = require'telescope.utils'
+
+local os_home = vim.loop.os_homedir()
 
 local M = {}
 
@@ -19,20 +22,35 @@ local function gen_from_z(opts)
   }
 
   local function make_display(entry)
+    local original = entry.path
+    local dir
+    if opts.tail_path then
+      dir = utils.path_tail(original)
+    elseif opts.shorten_path then
+      dir = utils.path_shorten(original)
+    else
+      dir = path.make_relative(original, opts.cwd)
+      if vim.startswith(dir, os_home) then
+        dir = '~/'..path.make_relative(dir, os_home)
+      elseif dir ~= original then
+        dir = './'..dir
+      end
+    end
+
     return displayer{
       {('%.2f'):format(entry.value), 'TelescopeResultsIdentifier'},
-      entry.path,
+      dir,
     }
   end
 
   return function(line)
-    local score_str, path = line:match'([%.%d]+)%s+(.+)'
+    local score_str, dir = line:match'([%.%d]+)%s+(.+)'
     local score = tonumber(score_str)
 
     return {
       value = score,
-      ordinal = path,
-      path = path,
+      ordinal = dir,
+      path = dir,
       display = make_display,
     }
   end
@@ -41,6 +59,7 @@ end
 M.list = function(opts)
   opts = opts or {}
   opts.cmd = utils.get_default(opts.cmd, {vim.o.shell, '-c', 'z -l'})
+  opts.cwd = utils.get_lazy_default(opts.cwd, vim.fn.getcwd)
   opts.entry_maker = utils.get_lazy_default(opts.entry_maker, function()
     return gen_from_z(opts)
   end)
@@ -57,15 +76,15 @@ M.list = function(opts)
       actions._goto_file_selection:replace(function(_, cmd)
         local entry = actions.get_selected_entry()
         actions.close(prompt_bufnr)
-        local path = from_entry.path(entry)
+        local dir = from_entry.path(entry)
         if cmd == 'edit' then
-          require'telescope.builtin'.find_files{cwd = path}
+          require'telescope.builtin'.find_files{cwd = dir}
         elseif cmd == 'new' then
-          vim.cmd('cd '..path)
-          print('chdir to '..path)
+          vim.cmd('cd '..dir)
+          print('chdir to '..dir)
         elseif cmd == 'vnew' then
-          vim.cmd('lcd '..path)
-          print('lchdir to '..path)
+          vim.cmd('lcd '..dir)
+          print('lchdir to '..dir)
         end
       end)
       return true
